6fa1ff304cf9a7751241ef8b615fb6f5d6c6a220
CEIBA\oscar.cruz <oscar.cruz@ceiba.com.co>
2022-01-26 18:20:10 -0500

    Ajustes pipeline agregar gradlew y ajuste sonar

--- Jenkinsfile
+++ Jenkinsfile
@@ -56,7 +56,7 @@ pipeline {
     }
     success {
       echo 'This will run only if successful'
-      junit '**/test-results/test/*.xml'
+      junit './microservicio/infraestructura/build/test-results/test/*.xml'
     }
     failure {
       echo 'This will run only if failed'
--- microservicio/aplicacion/src/main/java/com/ceiba/prestamo/comando/fabrica/FabricaPrestamo.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/prestamo/comando/fabrica/FabricaPrestamo.java
@@ -4,7 +4,6 @@ import com.ceiba.prestamo.comando.ComandoPrestamo;
 import com.ceiba.prestamo.modelo.entidad.Prestamo;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 
 @Component
 public class FabricaPrestamo {
@@ -15,17 +14,6 @@ public class FabricaPrestamo {
                 comandoPrestamo.getCedula(),
                 comandoPrestamo.getEquipoComputo(),
                 comandoPrestamo.getFechaCreacion(),
-                comandoPrestamo.getFechaEntrega(),
-                comandoPrestamo.getEstado()
-        );
-    }
-
-    public Prestamo Actualizar(ComandoPrestamo comandoPrestamo){
-        return new Prestamo(
-                comandoPrestamo.getId(),
-                comandoPrestamo.getCedula(),
-                "",
-                LocalDate.now(),
                 comandoPrestamo.getFechaEntrega(),
                 comandoPrestamo.getEstado()
         );
